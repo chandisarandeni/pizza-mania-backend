c298e5c154c6953ad72969eeaa7313f9553c6160
chandisarandeni <chandisarandeni2003@gmail.com>
2025-09-01 20:17:19 +0530

    init: springboot backend project ✅

---
+++ src/main/java/com/pizzamania/pizza_mania_backend/PizzaManiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pizzamania.pizza_mania_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaManiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaManiaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pizzamania/pizza_mania_backend/PizzaManiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzamania.pizza_mania_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaManiaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
