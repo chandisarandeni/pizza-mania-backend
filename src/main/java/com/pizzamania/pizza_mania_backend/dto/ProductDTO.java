1a55f307378d1ce5acb6e1d3d8f1e6c653a914a0
chandisarandeni <chandisarandeni2003@gmail.com>
2025-09-08 22:36:50 +0530

    feat: implement product dto ✅

---
+++ src/main/java/com/pizzamania/pizza_mania_backend/dto/ProductDTO.java
@@ -0,0 +1,18 @@
+package com.pizzamania.pizza_mania_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ProductDTO {
+    private String productId;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private String imageUrl;
+    private boolean isAvailable;
+}
